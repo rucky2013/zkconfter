f912158de4f84319c89377c7676e9f70a86e8346
linpn <lpn520@gmail.com>
2015-01-15 15:13:04 +0800

    Zookeeper configuration center

--- mvn-repository/github-deply.bat
+++ mvn-repository/github-deply.bat
@@ -1,2 +1,4 @@
 cd ../
-mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
+call mvn clean install -Dversion= -Dmaven.test.skip=true
+xcopy /Y target\*.jar mvn-repository\com\alibaba\zkconfter\1.0\
+xcopy /Y target\*.war mvn-repository\com\alibaba\zkconfter\1.0\
