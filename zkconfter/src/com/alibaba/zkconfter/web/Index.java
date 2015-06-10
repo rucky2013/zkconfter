492d94c5914cf810c1f27b78dd2264c2aa0e9cca
linpn <lpn520@gmail.com>
2015-06-10 18:03:04 +0800

    Zookeeper configuration center

---
+++ zkconfter/src/com/alibaba/zkconfter/web/Index.java
@@ -0,0 +1,22 @@
+package com.alibaba.zkconfter.web;
+
+import com.alibaba.zkconfter.client.ZkConfter;
+import com.alibaba.zkconfter.jsplite.mvc.Controller;
+import com.alibaba.zkconfter.jsplite.mvc.ModelView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Index implements Controller {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response,
+                        ModelView model) throws Exception {
+
+        ZkConfter zkConfter = new ZkConfter();
+        zkConfter.afterPropertiesSet();
+
+        model.addObject("hello", "钩吻");
+    }
+
+}
